75973bb987c05e488311282822711bc7b9f541f3
Vinnicius Eloi <vinniciuseloi@gmail.com>
2023-08-18 15:19:09 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
 //mais um console log de teste:
 console.log("O paulo fez essee commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
